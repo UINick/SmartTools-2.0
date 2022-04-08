bd2c92010ce24c3823c92d5e688ed7fcf2f8e031
UINick <nicholas_developer@outlook.com>
2022-04-07 21:59:56 -0300

    adding login button

--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/LoginDoMecanico.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/LoginDoMecanico.kt
@@ -2,10 +2,15 @@ package com.example.projetosmarttools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginDoMecanico : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_do_mecanico)
+    }
+
+    fun goToCadastroMecanico(v:View) {
+
     }
 }
