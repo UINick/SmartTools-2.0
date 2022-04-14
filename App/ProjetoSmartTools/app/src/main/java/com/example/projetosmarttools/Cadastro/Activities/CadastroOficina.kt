3ca8b6e4dc159a94f20686ce35c40904641fdba0
UINick <nicholas_developer@outlook.com>
2022-04-13 22:17:48 -0300

    getting cadastro textfileds

--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Cadastro/Activities/CadastroOficina.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Cadastro/Activities/CadastroOficina.kt
@@ -4,16 +4,64 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.example.projetosmarttools.Cadastro.Service.Oficina.CadastroOficina
+import com.example.projetosmarttools.Cadastro.Service.Oficina.CadastroOficinaVO
 import com.example.projetosmarttools.R
+import com.google.android.material.textfield.TextInputLayout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CadastroOficina : AppCompatActivity() {
+
+    lateinit var tiNome: TextInputLayout
+    lateinit var tiEmail: TextInputLayout
+    lateinit var tiCnpj: TextInputLayout
+    lateinit var tiTelefone: TextInputLayout
+    lateinit var tiSenha: TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_oficina)
+
+        tiNome = findViewById(R.id.ti_nome)
+        tiEmail = findViewById(R.id.ti_email)
+        tiCnpj = findViewById(R.id.ti_cnpj)
+        tiTelefone = findViewById(R.id.ti_telefone)
+        tiSenha = findViewById(R.id.ti_senha)
     }
 
     fun goToConfirmPassword(v:View) {
-        val confirmPassword = Intent(this, CadastroPassword:: class.java)
-        startActivity(confirmPassword)
+
+        Toast.makeText(baseContext, "Teste", Toast.LENGTH_SHORT).show()
+
+        val novaOficina = CadastroOficinaVO(
+            tiNome.editText?.text.toString(),
+            tiEmail.editText?.text.toString(),
+            tiSenha.editText?.text.toString(),
+            tiCnpj.editText?.text.toString(),
+            tiTelefone.editText?.text.toString(),
+        )
+
+        val postOficina = CadastroOficina.criar().post(novaOficina)
+
+        postOficina.enqueue(object : Callback<Void> {
+
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.code() == 201) {
+                    Toast.makeText(baseContext, "Criado", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(baseContext, "Erro na API", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Toast.makeText(baseContext, "Erro: ${t.stackTrace}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+//        val confirmPassword = Intent(this, CadastroPassword:: class.java)
+//        startActivity(confirmPassword)
     }
 }
--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Cadastro/Service/Oficina/CadastroOficinaVO.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Cadastro/Service/Oficina/CadastroOficinaVO.kt
@@ -2,6 +2,8 @@ package com.example.projetosmarttools.Cadastro.Service.Oficina
 
 data class CadastroOficinaVO(
     var nomeOficina: String,
+    var emailOficina: String,
+    var senhaOficina: String,
     val cnpjOficina: String,
     var telefoneOficina: String
 )
