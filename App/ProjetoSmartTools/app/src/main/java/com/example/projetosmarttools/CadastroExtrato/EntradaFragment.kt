9a0dd63beadc7f2e58e7ffcec2b35453da7bd17e
viniciushcano <vinicius.cano@bandtec.com.br>
2022-05-11 22:10:42 -0300

    add cadastros.fragments

---
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/EntradaFragment.kt
@@ -0,0 +1,13 @@
+package com.example.projetosmarttools.CadastroExtrato
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projetosmarttools.R
+
+
+class EntradaFragment : Fragment() {
+
+}
