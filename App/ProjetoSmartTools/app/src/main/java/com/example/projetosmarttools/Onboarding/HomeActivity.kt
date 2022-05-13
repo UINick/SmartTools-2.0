441158d3018cd06089c6cbcd8f7d44bc98442cab
Bruna Brito <bruna.melo@bandtec.com.br>
2022-05-12 21:46:05 -0300

    tela de ajustes

--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Onboarding/HomeActivity.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Onboarding/HomeActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.projetosmarttools.Ajustes
 import com.example.projetosmarttools.Login.Activities.LoginDoMecanico
 import com.example.projetosmarttools.R
 
