d953acc3765e7b98c4d0ce8f692cc02a1fe42d64
viniciushcano <vinicius.cano@bandtec.com.br>
2022-05-17 20:05:32 -0300

    create spinner

--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/EntradaFragment.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/EntradaFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.projetosmarttools.R
 
 
 class EntradaFragment : Fragment() {
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entrada, container, false)
+    }
 }
--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/SaidaFragment.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/SaidaFragment.kt
@@ -5,30 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.projetosmarttools.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SaidaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SaidaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +20,21 @@ class SaidaFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_saida, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SaidaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SaidaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spinner: Spinner = view.findViewById(R.id.sp_categoria_saida)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            view.context,
+            R.array.categoria_saida,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
+
+
 }
