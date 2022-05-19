21a3ae48ae0492d4037a6a590c9ed0ce7f1c1258
UINick <nicholas_developer@outlook.com>
2022-05-19 12:56:05 -0300

    adding cliente view

---
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Fragment/Cliente/ClienteVO.kt
@@ -0,0 +1,6 @@
+package com.example.projetosmarttools.Fragment.Cliente
+
+data class ClienteVO(
+    val nome: String,
+    val telefone: String
+)
--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Fragment/TabBarNavigation/TabPageAdapter.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Fragment/TabBarNavigation/TabPageAdapter.kt
@@ -3,6 +3,7 @@ package com.example.projetosmarttools.Fragment.TabBarNavigation
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.projetosmarttools.Fragment.Cliente.ClienteFragment
 import com.example.projetosmarttools.Fragment.Dash.DashFragment
 import com.example.projetosmarttools.Fragment.Extrato.ExtratoFragment
 
