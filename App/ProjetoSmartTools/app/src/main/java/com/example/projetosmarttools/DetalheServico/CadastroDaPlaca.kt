454cbf9e83b6668fe2cac891729c64d6509d3ff4
viniciushcano <vinicius.cano@bandtec.com.br>
2022-05-26 23:10:00 -0300

    criacao branch detalheServico

---
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/DetalheServico/CadastroDaPlaca.kt
@@ -0,0 +1,10 @@
+package com.example.projetosmarttools.DetalheServico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroDaPlaca : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/DetalheServico/DetalheVeiculoServico.kt
@@ -0,0 +1,10 @@
+package com.example.projetosmarttools.DetalheServico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheVeiculoServico : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
