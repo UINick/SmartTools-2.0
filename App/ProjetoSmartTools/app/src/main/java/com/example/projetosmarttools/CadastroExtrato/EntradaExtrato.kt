e312a87ce52bae47b2c63076056d746c8255f0f9
UINick <nicholas_developer@outlook.com>
2022-05-26 21:55:20 -0300

    adding new height for chart and comboBox working

--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/EntradaExtrato.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/EntradaExtrato.kt
@@ -5,13 +5,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import com.example.projetosmarttools.R
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.loading_screen.view.*
 
 class EntradaExtrato : AppCompatActivity() {
+
+    lateinit var spinner: Spinner
+    lateinit var categoria: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrada_extrato)
+
+        spinner = findViewById(R.id.sp_categoria_entrada)
+
+        val options = arrayOf("Manutenção", "Troca de óleo", "Funilaria", "Peças", "Pinturas")
+
+        spinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                categoria = options.get(p2)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                categoria = "Selecione uma opção"
+            }
+
+        }
     }
 }
--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/SaidaExtrato.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroExtrato/SaidaExtrato.kt
@@ -5,13 +5,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.example.projetosmarttools.R
 
 class SaidaExtrato : AppCompatActivity() {
+
+    lateinit var spinner: Spinner
+    lateinit var categoria: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_saida_extrato)
+
+        spinner = findViewById(R.id.sp_categoria_saida)
+
+        val options = arrayOf("Funcionario", "Contas", "Peças", "Outros")
+
+        spinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                categoria = options.get(p2)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                categoria = "Selecione uma opção"
+            }
+
+        }
     }
 }
