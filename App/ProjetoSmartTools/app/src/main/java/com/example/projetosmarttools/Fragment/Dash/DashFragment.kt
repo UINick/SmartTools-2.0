255707959e7792fd20da1f7af2ba1d584b748766
UINick <nicholas_developer@outlook.com>
2022-05-28 02:20:10 -0300

    chart working =)

--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Fragment/Dash/DashFragment.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Fragment/Dash/DashFragment.kt
@@ -8,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.FragmentContainerView
 import com.androidplot.pie.PieChart
 import com.androidplot.pie.Segment
@@ -25,6 +27,10 @@ import kotlinx.android.synthetic.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 
 class DashFragment : Fragment() {
@@ -32,19 +38,46 @@ class DashFragment : Fragment() {
     lateinit var pie:PieChart
     lateinit var botaoAdd: Button
     private lateinit var sessionManager: SessionManager
-    private var entradaClient: Double = 0.0
-    private var saidaClient: Double = 0.0
+    lateinit var entradaGeral: TextView
+    lateinit var saidaGeral: TextView
+    lateinit var saldoGeral: TextView
+    lateinit var eye: ImageView
+
+    private var setVisible: Boolean = false
+    private var entradaEye: Double = 0.0
+    private var saidaEye: Double = 0.0
+    private var saldoEye: Double = 0.0
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         pie = view.findViewById(R.id.pieChart)
         botaoAdd = view.findViewById(R.id.add)
+        saidaGeral = view.findViewById(R.id.saida_geral)
+        entradaGeral = view.findViewById(R.id.entrada_geral)
+        saldoGeral = view.findViewById(R.id.saldo_geral)
+        eye = view.findViewById(R.id.eye_icon)
         sessionManager = SessionManager(requireActivity().baseContext)
 
         botaoAdd.setOnClickListener {
             val novasTransacoes = Intent(view.context, NovasTransacoes::class.java)
             startActivity(novasTransacoes)
+        }
+
+        eye.setOnClickListener {
+            if (!setVisible) {
+                entradaGeral.text = "R$ ********"
+                saidaGeral.text = "R$ ********"
+                saldoGeral.text = "R$ ********"
+                setVisible = true
+            } else {
+                entradaGeral.text = "R$ ${entradaEye}"
+                saidaGeral.text = "R$ ${saidaEye}"
+                saldoGeral.text = "R$ ${roundOffDecimal(saldoEye)}"
+                setVisible = false
+            }
         }
 
 
@@ -80,10 +113,21 @@ class DashFragment : Fragment() {
                 ) {
                     LoadingScreen.hideLoading()
                     if (response.code() == 200) {
-                        opa(
-                            income = response.body()!!.valorTotalReceitas,
-                            outcome = response.body()!!.valorTotalDespesas
-                        )
+
+                        val conta = response.body()!!.valorTotalReceitas - response.body()!!.valorTotalDespesas
+
+                        saldoGeral.text = "R$ ${roundOffDecimal(conta)}"
+                        entradaGeral.text = "R$ ${response.body()!!.valorTotalReceitas}"
+                        saidaGeral.text = "R$ ${response.body()!!.valorTotalDespesas}"
+
+                        entradaEye = response.body()!!.valorTotalReceitas
+                        saidaEye = response.body()!!.valorTotalDespesas
+                        saldoEye = conta
+
+                            opa(
+                                income = response.body()!!.valorTotalReceitas,
+                                outcome = response.body()!!.valorTotalDespesas
+                            )
                     }
 
                 }
@@ -94,21 +138,30 @@ class DashFragment : Fragment() {
             })
     }
 
+    fun roundOffDecimal(number: Double): Double {
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.FLOOR
+        return df.format(number).toDouble()
+    }
+
     private fun opa(income: Double, outcome: Double) {
+
         val inPercent = (income * 100) / (income + outcome)
         val outPercent = (outcome * 100) / (income + outcome)
 
-        val entrada = Segment("${inPercent}%", income)
-        val saida = Segment("${outPercent}%", outcome)
+        val entrada = Segment("${inPercent.roundToInt()}%", income)
+        val saida = Segment("${outPercent.roundToInt()}%", outcome)
 
         val entradaCor = SegmentFormatter(Color.parseColor("#107C41"))
         val saidaCor = SegmentFormatter(Color.parseColor("#C60000"))
 
-        entradaCor.labelPaint.textSize = 100f
-        saidaCor.labelPaint.textSize = 100f
+        entradaCor.labelPaint.textSize = 45f
+        saidaCor.labelPaint.textSize = 45f
 
+        pie.clear()
         pie.addSegment(entrada, entradaCor)
         pie.addSegment(saida, saidaCor)
+        pie.redraw()
     }
 
     override fun onCreateView(
