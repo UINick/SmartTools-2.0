885a1d47377e2f7eaf63c033f9fd51ff39e55519
UINick <nicholas_developer@outlook.com>
2022-05-29 23:44:38 -0300

    find da placa com gambi

--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroVeiculo/VeiculoService.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/CadastroVeiculo/VeiculoService.kt
@@ -3,14 +3,15 @@ package com.example.projetosmarttools.CadastroVeiculo
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface VeiculoService {
 
     @POST("veiculos")
     fun postNewVeiculo(@Header("Authorization") token: String, @Body novoVeiculo: VeiculoVO): Call<Void>
+
+    @GET("veiculos")
+    fun fetchVeiculoByPlaca(@Header("Authorization") token: String, @Query("placa") placa: String): Call<VeiculoVO>
 
     companion object {
         var BASE_URL = "https://apinewsmarttools.herokuapp.com/"
--- App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Servicos/CadastroDaPlaca.kt
+++ App/ProjetoSmartTools/app/src/main/java/com/example/projetosmarttools/Servicos/CadastroDaPlaca.kt
@@ -4,20 +4,55 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 import com.example.projetosmarttools.CadastroExtrato.SaidaExtrato
 import com.example.projetosmarttools.CadastroVeiculo.CadastroVeiculo
+import com.example.projetosmarttools.CadastroVeiculo.VeiculoService
+import com.example.projetosmarttools.CadastroVeiculo.VeiculoVO
 import com.example.projetosmarttools.Clientes.CadastroCliente.CadastroClienteService
+import com.example.projetosmarttools.Fragment.Loading.LoadingScreen
 import com.example.projetosmarttools.R
+import com.example.projetosmarttools.SessionManager
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CadastroDaPlaca : AppCompatActivity() {
+
+    lateinit var search: EditText
+    private lateinit var sessionManager: SessionManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_da_placa)
+
+        sessionManager = SessionManager(this)
+        search = findViewById(R.id.search_placa_servico)
     }
 
     fun continueService(view: View) {
-        val servico = Intent(baseContext, DetalheVeiculoServico::class.java)
-        startActivity(servico)
+
+        LoadingScreen.displayLoadingWithText(this, "", false)
+
+        val request = VeiculoService.veiculo().fetchVeiculoByPlaca(
+            token = "Bearer ${sessionManager.fetchAuthToken()}",
+            placa = search.text.toString()
+        ).enqueue(object : Callback<VeiculoVO>{
+            override fun onResponse(call: Call<VeiculoVO>, response: Response<VeiculoVO>) {
+                    LoadingScreen.hideLoading()
+                    val servico = Intent(baseContext, DetalheVeiculoServico::class.java)
+                    startActivity(servico)
+            }
+
+            override fun onFailure(call: Call<VeiculoVO>, t: Throwable) {
+                LoadingScreen.hideLoading()
+                val veiculo = Intent(baseContext, CadastroVeiculo::class.java)
+                startActivity(veiculo)
+            }
+
+
+        })
     }
 
     fun cadastrarCarro(view: View) {
